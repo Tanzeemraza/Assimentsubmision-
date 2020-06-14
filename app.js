d544a743801f5b902455f894ec3f161a7e4346e1
tanzeemraza <tanzeemraza243@gmail.com>
2020-06-14 13:08:21 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// chapter 1
+// alert ("Error! Please Enter a valid Password");
+
+
+
+// var val1 = 15;
+// if (val1 < 16){
+//     alert ("Welcome to Js Land")
+//          }
+
+// chapter 2
+// var name = +prompt ("what is your name");
+// if (name == 14){
+//     alert("johen doe")
+// }else if (name < 14){
+//     alert("your age is 15")
+// }else{
+//     alert("certify")
+// }
+       
+// var a = +prompt ("enter your roll number");
+// if (a == 15){
+//     alert("example@xample.com")
+// }
+    
+// var a = "enter your value";
+// var b = "your value is";
+// var c = +prompt (a , b);
+// var d = a + 12;
+// document.write ("i can write html content throw Javascript:")
+
+
+// Chapter 3
+// var a = 25;
+// var b = 7;
+// var c = a + b;
+// console.log (c);
+
+// var t = +prompt ("enter your shirt price");
+// if (t == 200) {
+//     alert ("your shirt price is high")
+// }else if (t < 200){
+//    alert ("your shirt price is low")
+// }else{
+//     alert ("John doe buy T shirt from clothing store")
+// }
+
+// Chapter 4 
+// var a = 3.22
+// var b = 89%0
+// var c = nameofband
+// var d = home120
+// alert("Sorry, that's incorrect.")
+
+
+
+// var a = $_;
+// var b = "1";
+// var c = tooPot
+// alert("this is correct")
+
+// document.getElementById(ab).innerHTML;
+
+
